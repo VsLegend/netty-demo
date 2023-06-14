6ba7cbeef0fa0b9f0f1099b93616561b131bfc28
Wang junwei <wangjunwei@2woniu.com>
2023-06-14 17:08:08 +0800

    echo客户端服务端修改

--- ss-local/src/main/java/com/traning/local/proxy/ProxyClientRunner.java
+++ ss-local/src/main/java/com/traning/local/proxy/ProxyClientRunner.java
@@ -1,4 +1,13 @@
 package com.traning.local.proxy;
+
+import com.google.common.net.HostAndPort;
+import com.traning.local.echo.EchoClientRunner;
+import com.traning.runner.ClientRunner;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+
+import java.util.Scanner;
+
 /**
  * 客户端配置
  *
@@ -6,6 +15,41 @@ package com.traning.local.proxy;
  * @date 2021/7/21
  * @see <a href="https://netty.io/wiki/user-guide-for-5.x.html">netty</a>
  */
-public class ProxyClientRunner {
+public class ProxyClientRunner extends ClientRunner {
 
+    private HostAndPort targetProxy;
+
+    public static void main(String[] args) throws Exception {
+        // 代理服务器地址
+        HostAndPort hostAndPort = HostAndPort.fromString("127.0.0.1:7022");
+        ProxyClientRunner proxyClient = new ProxyClientRunner();
+        proxyClient.setHostAndPort(hostAndPort);
+        proxyClient.start();
+        ChannelFuture channelFuture = proxyClient.getChannelFuture();
+
+        // 设置目标服务器地址
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入目标服务器地址：");
+        String next = scanner.next();
+        HostAndPort proxy = HostAndPort.fromString(next);
+        proxyClient.setTargetProxy(proxy);
+
+        // 执行一次服务访问
+        channelFuture.channel().writeAndFlush("");
+    }
+
+    @Override
+    public ChannelHandler[] createChannelHandlers() {
+        return new ChannelHandler[] {
+
+        };
+    }
+
+    public HostAndPort getTargetProxy() {
+        return targetProxy;
+    }
+
+    public void setTargetProxy(HostAndPort targetProxy) {
+        this.targetProxy = targetProxy;
+    }
 }
